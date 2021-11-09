0a05bfd946818f999ca2fca95ee547422a2a89c6
aria <aryashahbazi@gmail.com>
2021-11-09 16:17:08 +0330

    I add tow directory (python/Winner-Tshirt/)

---
+++ python/Winner-Tshirt/winnertishert.py
@@ -0,0 +1,42 @@
+
+ 
+size_sarshoneh_ghad = input("Enter a size of T-shirt:").split()
+shonehsize = range(int(size_sarshoneh_ghad[0])+1)
+hight = range(int(size_sarshoneh_ghad[1])+1)
+
+
+
+sizewinner = input("Enter a size of T-shirt:").split()
+shonehsize1 = int(sizewinner[0])
+hight1 = int(sizewinner[1])
+
+
+result = []
+for i in shonehsize:
+    if shonehsize1 in shonehsize:
+        result.append('yes')
+        break
+    else:
+        result.append('no')
+        break
+for i in hight:
+    if hight1 in hight:
+        result.append('yes')
+        break
+    else:
+        result.append('no')
+        break
+
+
+print(result)
+if __name__ == '__main__':
+    counter = 0
+    for i in result:
+        if i == 'yes':
+            counter += 1
+        else:
+            counter = 0
+    if counter == 2:
+        print('yes')
+    else:
+        print('no')
